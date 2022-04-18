22445727cd553a86b692c051baa4e1a1439c7756
KakushaVenecia <kakushavenecia@gmail.com>
2022-04-19 01:41:02 +0300

    This will make changes to the environment Production file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  VENAPI: 'https://api.github.com/users/KakushaVenecia',
+  APIURL: 'https://api.github.com/users/',
+  APIKEY: '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',
+  REPOURL:
+    'https://api.github.com/users/{user}/repos{?type,page,per_page,sort}',
+  production: true,
 };
