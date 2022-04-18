25aecd38200eff7b7f66cc1777bad3503608d4a8
KakushaVenecia <kakushavenecia@gmail.com>
2022-04-19 01:16:45 +0300

    This will add the environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  VENAPI: 'https://api.github.com/users/KakushaVenecia',
+  APIURL: 'https://api.github.com/users/',
+  APIKEY: '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',
+  REPOURL:'https://api.github.com/users/{user}/repos{?type,page,per_page,sort}',
+  production: false,
 };
 
 /*
