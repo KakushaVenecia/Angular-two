8aac4585ac647e1e5b4aae3f5a26a8b6bef9e09a
KakushaVenecia <kakushavenecia@gmail.com>
2022-04-19 01:10:46 +0300

    This will add the models component

---
+++ src/app/models/Repo.ts
@@ -0,0 +1,8 @@
+export interface Repo {
+  newUserName: string;
+  avatar: string;
+  bio: string;
+  followers: number;
+  following: number;
+  repos: string;
+}
---
+++ src/app/models/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+  avatar_url:string;
+  login: string;
+  name: string;
+  avatar: string;
+  bio: string;
+  followers: number;
+  following: number;
+  created_at:string;
+  public_repos: string;
+}
